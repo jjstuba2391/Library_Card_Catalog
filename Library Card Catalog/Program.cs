f1c8632c4e6dfc06577cbd3259f97e5100da3acc
Adrian Ledesma <ledadrian@gmail.com>
2017-09-28 22:28:31 -0500

    Finished Main Menu. Set variables in public class Book. Added dialog for CardCatalog functions (but doesn't do anything yet)

--- Library Card Catalog/Program.cs
+++ Library Card Catalog/Program.cs
@@ -15,35 +15,63 @@ namespace Library_Card_Catalog
             //2. Add a Book
             //3. Save and Exit
             //  Re-display Menu until 'Save and Exit' is chosen.
+            Console.WriteLine("Welcome to the our Library!\n");
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1 - List all books");
+            Console.WriteLine("2 - Add a book");
+            Console.WriteLine("3 - Save and Exit");
 
+            int selection = Convert.ToInt32(Console.ReadLine());
 
-        } 
+            //if (selection == 1)
+            //{
+            //    CardCatalog.Listbooks(); //Need to create this function
+            //}
+            //else if (selection == 2)
+            //{
+            //    CardCatalog.AddBook(); //Need to create this function
+            //}
+            //else if (selection == 3)
+            //{
+            //    SaveAndExit??? //need to research and create this function
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid Input");
+            //    //need to figure out how to return to main menu;
+            //}
+        }
     }
     public class Book
     {
         //Contains properties of books
         //  Title, Author, other properties
+        private string Title = "";
+        private string Author = "";
+        private string Genre = "";
     }
     public class CardCatalog
     {
         //Logic goes here
         private string _filename;
-        private string books;
+        private string _books;
         public CardCatalog(string fileName)
         {
             _filename = fileName;
         }
         public void Listbooks()
-        {// funtion to list all books
-
+        {
+            // funtion to list all books
         }
         public AddBook(string author, string title, string genre)
         {
             //function to add all books
+            Console.WriteLine("Book added"); //returns to main menu
         }
         public Save()
         {
             //adding to list
+            Console.WriteLine("Book Saved"); //returns to main menu
         }
     }
 }
