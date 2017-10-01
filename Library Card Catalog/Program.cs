58e3584a18836f7a3b3d1cb3dc7ef871a59d307c
Adrian Ledesma <ledadrian@gmail.com>
2017-10-01 16:21:22 -0500

    More Reorganizing

--- Library Card Catalog/Program.cs
+++ Library Card Catalog/Program.cs
@@ -23,12 +23,12 @@ namespace Library_Card_Catalog
                 Console.WriteLine("2 - Add a book");
                 Console.WriteLine("3 - Save and Exit");
 
-                selection = Convert.ToInt32(Console.ReadLine());
+                PromptForNumber();
 
                 switch (selection)
                 {
                     case 1:
-                        // CardCatalog.ListBooks();
+                        CardCatalog.ListBooks();
                         break;
 
                     case 2:
@@ -52,7 +52,20 @@ namespace Library_Card_Catalog
                         break;
 
                 }
-            }    
+            }
+        }
+        private static int PromptForNumber(string promptString = "Enter a number: ")
+        {
+            int selection = 0;
+            string input = "";
+
+            while (!int.TryParse(input, out selection))
+            {
+                Console.WriteLine(promptString);
+                input = Console.ReadLine();
+            }
+
+            return selection;
         }
     }
 }
