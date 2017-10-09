b0034382d5447beee9a4d132bba0f7f0252edd07
Aledes <ledadrian@gmail.com>
2017-10-09 12:22:54 -0500

    Fixed/Completed this program

--- Library Card Catalog/CardCatalog.cs
+++ Library Card Catalog/CardCatalog.cs
@@ -13,48 +13,51 @@ namespace Library_Card_Catalog
 
     public class CardCatalog
     {
-        public static List<Book> booklist = new List<Book>();
+        private List<Book> books;
         private string _filename;
-        //private string _books;
+
         public CardCatalog(string fileName)
         {
             _filename = fileName;
-        }
-        public static void AddBook(string title, string author, string genre)
-        {
-
-            Book entry = new Book(title, author, genre);
-            booklist.Add(entry);
-
-            Console.WriteLine("Book added. Restart application to see changes!\n"); //returns to main menu
-        }
-        public static void ListBooks()
-        {
-            Console.WriteLine("\nBooks Listed:");
-            //Deserializer
-            using (Stream stream = File.Open("MyFile.bin", FileMode.Open))
+            if (File.Exists(fileName))
             {
-                BinaryFormatter bin = new BinaryFormatter();
-                var booklist = (List<Book>)bin.Deserialize(stream);
-
-                foreach (Book entry in booklist)
-                {
-                    Console.WriteLine("\n{0}, by {1}. Genre: {2}\n",
-                        entry.Title,
-                        entry.Author,
-                        entry.Genre);
-                }
-
+                Stream stream = new FileStream(_filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+                IFormatter formatter = new BinaryFormatter();
+                books = (List<Book>)formatter.Deserialize(stream);
             }
-            Console.WriteLine("No More Entries.\nReturning to Menu...");
+            else
+            {
+                books = new List<Book>();
+            }
         }
-        public static void SaveAndExit()
+        public void AddBook(string title, string author, string genre)
+        {
+            Book newBook = new Book
+            {
+                Title = title,
+                Author = author,
+                Genre = genre
+            };
+        books.Add(newBook);
+        }
+        public void ListBooks()
+        {
+            Console.Clear();
+            foreach(var book in books)
+            {
+                Console.WriteLine("Title: {0}\t Author: {1}\t Genre: {2}", book.Title, book.Author, book.Genre);
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+        public void SaveAndExit()
         {
             //Serializer
+            Stream stream = new FileStream(this._filename, FileMode.Create, FileAccess.Write, FileShare.None);
             IFormatter formatter = new BinaryFormatter();
-            Stream stream = new FileStream("MyFile.bin", FileMode.Create, FileAccess.Write, FileShare.None);
-            formatter.Serialize(stream, booklist);
+            formatter.Serialize(stream, this.books);         
             stream.Close();
+            stream.Dispose();
             Console.WriteLine("Book(s) Saved. Good Bye!");
             Console.ReadLine();
         }
